9e6c2b687a287d17dd619927933dc3f38f8e9c91
Jack Malcom <j@ckmal.com>
2025-08-04 17:55:17 -0500

    improve attending readability

--- src/routes/event/[id]/+page.svelte
+++ src/routes/event/[id]/+page.svelte
@@ -41,9 +41,9 @@
 			${
 				// janky but it works!
 				attendButtonText === 'Attending'
-					? 'bg-green-500 hover:bg-green-600'
+					? 'bg-green-600'
 					: attendButtonText === 'Cancel'
-						? 'bg-red-500 hover:bg-red-600'
+						? ' bg-red-600'
 						: 'bg-blue-500 hover:bg-blue-600'
 			}`}
 			onmouseenter={() =>
